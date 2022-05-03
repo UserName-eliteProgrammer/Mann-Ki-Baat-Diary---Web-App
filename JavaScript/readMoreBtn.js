12ea355bda3fde90d933ed2d6ea4f2696dd8aab6
Manish Sharma <sharmamanish16111999@gmail.com>
2022-05-03 19:02:54 +0530

    Ready, but i want to add one more feature

--- JavaScript/readMoreBtn.js
+++ JavaScript/readMoreBtn.js
@@ -1,42 +1,50 @@
-
+let prevHTML = document.querySelector("body").innerHTML;
 
 function readMore(id) {
-    id = Number(id.slice(12));
-    console.log(id);
-    const cardsArr = JSON.parse(localStorage.getItem("key"));
-    const titleArr = JSON.parse(localStorage.getItem("titleKey"));
+  id = Number(id.slice(12));
+  const cardsArr = JSON.parse(localStorage.getItem("key"));
+  const titleArr = JSON.parse(localStorage.getItem("titleKey"));
 
+  for (let i = 0; i < cardsArr.length; i++) {
+    if (i == id) {
+      let modalContent = cardsArr[i];
+      let titleOfModal = titleArr[i];
 
-    for (let i = 0; i < cardsArr.length; i++) {
-        if (i == id) {
-            let modalContent = cardsArr[i];
-            let titleOfModal = titleArr[i];
-            document.querySelector(
-              "body"
-            ).innerHTML += `<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Launch demo modal
+      document.querySelector("body").innerHTML =
+        prevHTML +
+        `<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" id="fireModalBtn" style="display: none">
+  
 </button>
 
 <!-- Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
+  <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">${titleOfModal}</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <button type="button" class="btn-close closeModalBtn" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
         ${modalContent}
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary closeModalBtn" data-bs-dismiss="modal">Close</button>
       </div>
     </div>
   </div>
 </div>`;
-            break;
-        }
+      const fireModalBtn = document.getElementById("fireModalBtn");
+      fireModalBtn.click();
+      break;
     }
+  }
 }
+
+if (document.querySelector(".closeModalBtn")) {
+  console.log("fired");
+  document.querySelector(".closeModalBtn").addEventListener("click", () => {
+    document.querySelector("body").innerHTML = prevHTML;
+  });
+}
+
